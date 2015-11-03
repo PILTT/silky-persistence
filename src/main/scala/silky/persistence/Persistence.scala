2f977ae1c31979605e0e6bf082f56344fbb3bc1d
Franck Rasolo <franck.rasolo@gmail.com>
2015-11-03 16:36:33 +0000

    First stab at defining the API

---
+++ src/main/scala/silky/persistence/Persistence.scala
@@ -0,0 +1,12 @@
+package silky.persistence
+
+trait Persistence {
+  def initialise(contexts: String*): Unit
+  def lastRefAcross(prefix: Char, contexts: String*): String
+  def save(entry: Entry): Entry
+  def find(context: String, ref: String): Option[Entry]
+  def load(context: String, predicate: String ⇒ Boolean): Seq[Entry]
+  def move(ref: String, source: String, target: String): Unit
+}
+
+case class Entry(context: String, ref: String, contents: String)
