c7583877d25c33374b07969b773a460ba6fb9ee2
Franck Rasolo <franck.rasolo@gmail.com>
2015-11-03 22:56:28 +0000

    Deleted superfluous methods

--- src/main/scala/silky/persistence/file/Filepath.scala
+++ src/main/scala/silky/persistence/file/Filepath.scala
@@ -4,17 +4,9 @@ import java.nio.charset.StandardCharsets._
 import java.nio.file.Files._
 import java.nio.file.StandardCopyOption._
 import java.nio.file.StandardOpenOption._
-import java.nio.file.{Paths, Files, Path}
-
-import scala.reflect.io.File
+import java.nio.file.{Files, Path}
 
 object Filepath {
-  def save(content: String, path: Path)   = write(path, content.getBytes(UTF_8), CREATE, WRITE, TRUNCATE_EXISTING)
-  def append(content: String, path: Path) = write(path, content.getBytes(UTF_8), CREATE, WRITE, APPEND)
-
-  def move(source: Path, target: Path): Path = Files.move(source, target, ATOMIC_MOVE)
-  def move(source: File, target: File): Path = move(Paths.get(source.path), Paths.get(target.path))
-
-  def create(path: Path) = createFile(path)
-  def load(path: Path)   = File(path.toFile).slurp()
+  def save(content: String, path: Path): Path = write(path, content.getBytes(UTF_8), CREATE, WRITE, TRUNCATE_EXISTING)
+  def move(source: Path, target: Path):  Path = Files.move(source, target, ATOMIC_MOVE)
 }
