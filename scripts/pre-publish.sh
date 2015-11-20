145bf35eda8d2ada9c0653f59a5b18a2520ef369
Franck Rasolo <franck.rasolo@gmail.com>
2015-11-20 17:39:18 +0000

    Made Travis build create database user and schema in PostgreSQL

--- scripts/pre-publish.sh
+++ scripts/pre-publish.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 openssl des3 -d -salt -in ./scripts/sonatype.asc.enc -out ./scripts/sonatype.asc -k "$SECRET"
+
+psql -c "CREATE USER silky-app WITH CREATEDB;" -U postgres
+psql -c "CREATE DATABASE silky-db;" -U silky-app
