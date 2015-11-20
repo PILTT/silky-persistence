0623fb181502c4bacad6c148ea7265730936f911
Franck Rasolo <franck.rasolo@gmail.com>
2015-11-20 17:48:22 +0000

    Trying the createuser utility instead

--- scripts/pre-publish.sh
+++ scripts/pre-publish.sh
@@ -2,5 +2,9 @@
 
 openssl des3 -d -salt -in ./scripts/sonatype.asc.enc -out ./scripts/sonatype.asc -k "$SECRET"
 
-psql -c "CREATE USER silky-app WITH CREATEDB;" -U postgres
+createuser silky-app
+#createdb --owner=silky-app --encoding=UTF8 silky-db
+
+#psql -c "CREATE USER silky-app WITH CREATEDB;" -U postgres
 psql -c "CREATE DATABASE silky-db;" -U silky-app
+#psql -c "ALTER DATABASE silky-db OWNER TO silky-app;" -U postgres
