7a86911d6dc0f48ad3c4922e8105a2ac3914b264
Franck Rasolo <franck.rasolo@gmail.com>
2015-11-20 17:56:44 +0000

    Trying quoted values

--- scripts/pre-publish.sh
+++ scripts/pre-publish.sh
@@ -2,9 +2,5 @@
 
 openssl des3 -d -salt -in ./scripts/sonatype.asc.enc -out ./scripts/sonatype.asc -k "$SECRET"
 
-createuser silky-app
-#createdb --owner=silky-app --encoding=UTF8 silky-db
-
-#psql -c "CREATE USER silky-app WITH CREATEDB;" -U postgres
-psql -c "CREATE DATABASE silky-db;" -U silky-app
-#psql -c "ALTER DATABASE silky-db OWNER TO silky-app;" -U postgres
+psql -c "CREATE USER 'silky-app' WITH CREATEDB;" -U postgres
+psql -c "CREATE DATABASE 'silky-db';" -U silky-app
