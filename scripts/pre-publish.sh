956a98ec493b58123c4129aa2090bf7ffe63f43d
Franck Rasolo <franck.rasolo@gmail.com>
2015-11-20 19:01:28 +0000

    Updated commands to create user and database

--- scripts/pre-publish.sh
+++ scripts/pre-publish.sh
@@ -2,5 +2,5 @@
 
 openssl des3 -d -salt -in ./scripts/sonatype.asc.enc -out ./scripts/sonatype.asc -k "$SECRET"
 
-psql -c "CREATE USER 'silky-app' WITH CREATEDB;" -U postgres
-psql -c "CREATE DATABASE 'silky-db';" -U silky-app
+psql -c "CREATE USER silky_app WITH CREATEDB;"
+psql -c "CREATE DATABASE silky_db WITH ENCODING 'UTF8' OWNER silky_app;"
