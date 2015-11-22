8b081c2f37941920bbe53887b01b4b869274e624
Franck Rasolo <franck.rasolo@gmail.com>
2015-11-22 19:37:27 +0000

    Simplified assertions using more concise matchers for Future values

--- src/test/scala/silky/persistence/postgresql/PostgresPersistenceSpec.scala
+++ src/test/scala/silky/persistence/postgresql/PostgresPersistenceSpec.scala
@@ -1,15 +1,15 @@
 package silky.persistence.postgresql
 
-import org.scalatest.MustMatchers._
-import org.scalatest.OptionValues._
 import org.scalatest.concurrent.ScalaFutures._
 import org.scalatest.exceptions.TestFailedException
 import org.scalatest.time.{Millis, Seconds, Span}
 import org.scalatest.{BeforeAndAfterAll, WordSpec}
 import silky.persistence.Entry
 import silky.persistence.postgresql.PostgresDriver.api._
+import silky.persistence.FutureMatchers._
 
 class PostgresPersistenceSpec extends WordSpec with BeforeAndAfterAll {
+  implicit val execution = concurrent.ExecutionContext.global
   implicit def patienceConfig = PatienceConfig(timeout = Span(2, Seconds), interval = Span(50, Millis))
 
   private val db = Database.forURL(
@@ -17,7 +17,7 @@ class PostgresPersistenceSpec extends WordSpec with BeforeAndAfterAll {
     driver = "org.postgresql.Driver",
     user   = "silky_app")
 
-  private val persistence = new PostgresPersistence(db)(concurrent.ExecutionContext.global)
+  private val persistence = new PostgresPersistence(db)
 
   private val message1 :: ticket1 :: ticket2 :: ticket3 :: ticket4 :: xs = Seq(
     Entry("messages", "M00000001", """{ "metadata": { "status": "Pending" }, "message": "Hello World!" }"""),
@@ -47,38 +47,39 @@ class PostgresPersistenceSpec extends WordSpec with BeforeAndAfterAll {
   override protected def afterAll() = { persistence.dropSchema().futureValue; db.close() }
 
   "lastRefAcross returns the last reference across multiple contexts" in {
-    persistence.lastRefAcross(prefix = 'T', "deleted", "incoming", "tickets").futureValue mustBe "00000015"
+    persistence.lastRefAcross(prefix = 'T', "deleted", "incoming", "tickets") willReturn "00000015"
   }
 
   "lastRefAcross returns '00000000' when no matching entries are found in a given set of contexts" in {
-    persistence.lastRefAcross(prefix = 'T', "one", "two", "three").futureValue mustBe "00000000"
+    persistence.lastRefAcross(prefix = 'T', "one", "two", "three") willReturn "00000000"
   }
 
   "find returns an entry existing in a given context" in {
-    persistence.find("messages", "M00000001").futureValue.value.ref mustBe message1.ref
+    persistence.find("messages", "M00000001").map(_.get.ref) willReturn message1.ref
   }
 
   "find returns none for a non-existent entry in a given context" in {
-    persistence.find("messages", "M00000002").futureValue mustBe None
+    persistence.find("messages", "M00000002") willReturn None
   }
 
   "load returns entries whose reference matches a given predicate in a given context" in {
-    val tickets = persistence.load("tickets", predicate = _.matches("T0000000[1-4]")).futureValue
-    tickets.map(_.ref) must contain only (ticket2.ref, ticket3.ref)
+    val tickets = persistence.load("tickets", predicate = _.matches("T0000000[1-4]"))
+    tickets.map(v ⇒ v.map(_.ref)) will contain only (ticket2.ref, ticket3.ref)
   }
 
   "move simply moves the file containing the given entry from one directory (context) to another" in {
-    val moved = persistence.move(ticket4.ref, "incoming", "tickets").futureValue
-    (moved.context, moved.ref) mustBe ("tickets", ticket4.ref)
+    val moved = persistence.move(ticket4.ref, "incoming", "tickets")
+    moved.map(v ⇒ (v.context, v.ref)) willReturn ("tickets", ticket4.ref)
   }
 
   "save writes updated contents to the database" in {
     val updated = message1.copy(contents = """{"message": "Hello New World!"}""")
     persistence.save(updated).futureValue
-    persistence.find(message1.context, message1.ref).futureValue.value mustBe updated
+    persistence.find(message1.context, message1.ref) willReturn updated
   }
 
   "save cannot update an entry with a different context but the same ref" in {
+    import org.scalatest.MustMatchers._
     a [TestFailedException] mustBe thrownBy (persistence.save(message1.copy(context = "foo")).futureValue)
   }
 }
